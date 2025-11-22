d6959557402e1789f1b1b354163c2a63ae486173
xl <574024225@qq.com>
2025-11-22 21:58:05 +0800

    add maximize plugin

--- nvim_self/nvim/lua/plugins/maximize.lua
+++ nvim_self/nvim/lua/plugins/maximize.lua
@@ -1,6 +1,29 @@
 return {
-  "szw/vim-maximizer",
-  keys = {
-    { "<leader>z", "<cmd>MaximizerToggle<CR>", desc = "maximize" },
+  {
+    "declancm/maximize.nvim",
+    config = true,
+    keys = {
+      {
+        "<leader>zt",
+        function()
+          require("maximize").toggle()
+        end,
+        desc = "window maximize toggle",
+      },
+      {
+        "<leader>zm",
+        function()
+          require("maximize").maximize()
+        end,
+        desc = "window maximize",
+      },
+      {
+        "<leader>zr",
+        function()
+          require("maximize").restore()
+        end,
+        desc = "window restore",
+      },
+    },
   },
 }
